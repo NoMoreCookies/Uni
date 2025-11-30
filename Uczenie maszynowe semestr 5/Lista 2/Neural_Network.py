33dd066c098854a070fc5d78c41f3757a9692aa1
Kacper <kacperszmigielski123@gmail.com>
2025-11-30 15:53:34 +0100

    skończenie zadania drugiego z sieci neuronowych

--- Uczenie maszynowe semestr 5/Lista 2/Neural_Network.py
+++ Uczenie maszynowe semestr 5/Lista 2/Neural_Network.py
@@ -1,6 +1,11 @@
 import numpy as np
 import random
 
+# Kod został napisany na podstawie książki "Neural Networks and Deep Learning"
+# Michaela Nielsena, 
+# Wzory w niej przedstawione są zgodne z wykładowymi, z tą różnicą, że tutaj
+# mamy biasy oddzielnie, gdzie w wykładach są one w macierzach wag
+
 
 class Network(object):
     def __init__(self, sizes):
@@ -11,151 +16,235 @@ class Network(object):
         Args:
             sizes (list of ints)  - lengths of hiddens layers
         """
+
         # number of hidden layers
         self.num_layers = len(sizes)
 
         # random generated biases
+        # (from sizes[1] because input layer has no biases)
         self.biases = [np.random.randn(y, 1)
                        for y in sizes[1:]]
-        
-        # random generated weights
-        self.weights = [np.random.randn(y, x)
-                        for x, y in zip(sizes[:-1], sizes[1:])]
+             
+        # random generated weights (shift is because weights are between
+        # layers and we need matrix multiplication for forward propagation)
+        self.weights = [np.random.randn(y, a)
+                        for a, y in zip(sizes[:-1], sizes[1:])]
            
-    def feedforward(self, a):
-        """ FeedForward func 
+    def feedForward(self, a):
+        """ FeedForward function
 
-        It is responsible for calculating actiations for 
+        It is responsible for calculating activations for
         next neureons in our neural network
 
         Args:
             self (Network) : Object
             a (float list) : Outputs fromm last layers
         """
-        for b, w in zip(self.biases, self.weights):
+        for bias, weight in zip(self.biases, self.weights):
 
             # calculating with sigmoid function
-            a = sigmoid(np.dot(w, a)+b)
+            # (sigmoid returns numbers bewteen 0 and 1)
+            # 0 when numbers are very negative
+            # and 1 when numbers are very positive
+            a = sigmoid(np.dot(weight, a)+bias)
 
         return a
         
-    def SGD(self, training_data, epochs, mini_batch_size, eta, test_data=None):
+    def StochasticGradientDescent(self, training_data, epochs,
+                                    trainSampleSize, eta, test_data=None):
         """Stochastic Gradient Descent
         
-        Finds local minima for weights and biases of our neural network 
+        Finds local minima for weights and biases of our neural network
         using Stochastic Gradient Descent Algorithm
 
         Args:
             self (Network) : Object
             training_data (list of pairs) : Image and number on image
             epochs (int) :  Number of learning steps of our network
-            mini_batch_size (int) :  Length of batches that training data should be divied into
-            eta (float) : Length of our SGD step 
+            trainSampleSize (int) :  Length of batches that
+            training data should be divied into
+            eta (float) : Length of our StochasticGradientDescent step
             test_data (list of pairs) : Image and number on image
         
         """
-
-        if test_data:
-            n_test = len(test_data)
 
         n = len(training_data)
 
         for j in range(epochs):
 
-            #random training data shuffle
+            # random training data shuffle
             random.shuffle(training_data)
 
             # rendering list of mini batches
-            mini_batches = [
-                training_data[k:k+mini_batch_size] for k in range(0, n, mini_batch_size )
-                ] 
+            miniSamples = [
+                training_data[k: k+trainSampleSize] for
+                k in range(0, n, trainSampleSize)
+                ]
             
-            # teaching our nneural network on with every batch
-            for mini_batch in mini_batches:
-                self.update_mini_batch(mini_batch, eta)
+            # teaching our neural network with every training sample
+            for mini_sample in miniSamples:
+                self.updateBasedOnMiniSample(mini_sample, eta)
 
             # evaluating performance of our neural network
             # after previous learning step
             if test_data:
-                print("Epoch {0}: {1} / {2}".format(j, self.evaluate(test_data),n_test))
+                print("Step {0}: score {1} ".format(j, self.score(test_data)))
             else:
-                print("Epoch {0} complete".format(j))
+                print("Step number: {0} completed".format(j))
 
-    def update_mini_batch(self, mini_batch, eta):
-
-        # dla każdej warstwy w każdej sieci są biases
-        nabla_b = [np.zeros(b.shape) for b in self.biases]
-
-        # dla każdej warstwy w każdej sieci są biases
-        nabla_w = [np.zeros(w.shape) for w in self.weights]
-
-        for x, y in mini_batch:
-            # propagacja wsteczna
-            delta_nabla_b, delta_nabla_w = self.backprop(x, y)
-
-
-            nabla_b = [nb + dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
-
-
-            nabla_w = [nw + dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
-
-        # update biasów i wag
-
-        self.weights = [w - (eta/len(mini_batch))*nw
-                        for w, nw in zip(self.weights, nabla_w)]
+    def updateBasedOnMiniSample(self, mini_sample, eta):
+        """"Updates Based On Mini Sample
         
-        self.biases = [b - (eta/len(mini_batch))*nb
-                       for b, nb in zip(self.biases, nabla_b)]
+        Updates weights and biases of our neural network
+        using gradient descent from backpropagation
+        
+        Args:
+            self (Network) : Object
+            mini_sample (list of pairs) : Image and number on image
+            eta (float) : Length of our StochasticGradientDescent step
+        
+        """
+
+        # for each layer of biases we create zero vectors
+        dC_db = [np.zeros(b.shape) for b in self.biases]
+
+        # for each layer of weights we create zero matrices
+        dC_dw = [np.zeros(w.shape) for w in self.weights]
+
+        for a, y in mini_sample:
+
+            # backpropagation
+            delta_dC_db, delta_dC_dw = self.backPropagation(a, y)
+
+            # sum of gradients from all samples in mini batch
+            dC_db = [nb + dnb for nb, dnb in zip(dC_db, delta_dC_db)]
+
+            dC_dw = [nw + dnw for nw, dnw in zip(dC_dw, delta_dC_dw)]
+
+        # updating weights and biases using mean from mini batch gradients
+        self.weights = [weight - (eta/len(mini_sample))*nweight
+                        for weight, nweight in zip(self.weights, dC_dw)]
+        
+        self.biases = [bias - (eta/len(mini_sample))*nbias
+                       for bias, nbias in zip(self.biases, dC_db)]
     
-    def backprop(self, x, y):
+    def backPropagation(self, x, y):
+        """Backpropagation
 
-        # tworzymy wetkroy zer dla b 
-        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        Calculates gradient of cost_derivative function with respect to
+        weights and biases using backpropagation algorithm
 
-        # tworzymy wetkroy zer dla b 
-        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        Args:
+            self (Network) : Object
+            x (float list) : Input image
+            y (float list) : Expected output
+        """
 
-        # aktywacje
+        # vekctors of zeros for biases
+        dC_db = [np.zeros(b.shape) for b in self.biases]
+
+        # matrices of zeros for weights
+        dC_dw = [np.zeros(w.shape) for w in self.weights]
+
+        # activations
         activation = x
+
+        # we need to remember all activations  for gradient descent
         activations = [x]
-        zs = []
+        
+        # outputs of all layers before activation function
+        z_layers = []
 
-        for b, w in zip(self.biases, self.weights):
-            # przemnazamy aktywacje prze wagi i dodajemy bias
-            z = np.dot(w,activation)+b
-            zs.append(z)
+        for bias, weight in zip(self.biases, self.weights):
 
-            # przepuszczamy przez funckje sigmoid
+            # we calculate layers before activation
+            z = np.dot(weight, activation) + bias
+            z_layers.append(z)
+
+            # we put it through sigmoid function
             activation = sigmoid(z)
+
+            # we store it to activations
             activations.append(activation)
 
-        
-        delta = self.cost_derivative(activations[-1], y) * sigmoid_prime(zs[-1] )
+        # backward pass
+        delta = self.cost_derivative(activations[-1], y) *\
+            sigmoid_derivative(z_layers[-1])
 
-        nabla_b[-1] = delta
-        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
-
+        # we calculate new weights and biases for last layer
+        dC_db[-1] = delta
+        dC_dw[-1] = np.dot(delta, activations[-2].transpose())
+ 
+        # now we need to calculate them for inner layers,
+        # we use chain formula to get the derivatives
         for l in range(2, self.num_layers):
-            z = zs[-l]
-            sp = sigmoid_prime(z)
-            delta = np.dot(self.weights[-l+1].transpose(),delta) * sp
-            nabla_b[-l] = delta
-            nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
-        return (nabla_b, nabla_w)
+
+            # we start from second to last layer
+            z = z_layers[-l]
+
+            # we calc derative of sigmoid function from outputs before activation
+            sigm_der = sigmoid_derivative(z)
+
+            # we calculate delta for current layer
+            delta = np.dot(self.weights[-l+1].transpose(), delta) * sigm_der
+
+            # we update biases and weights gradients
+            dC_db[-l] = delta
+
+            dC_dw[-l] = np.dot(delta, activations[-l-1].transpose())
+
+        return (dC_db, dC_dw)
     
-    def evaluate(self, test_data):
-        test_results = [(np.argmax(self.feedforward(x)), y) 
-                        for (x, y) in test_data]
-        return sum(int(x == y) for (x, y) in test_results)
+    def score(self, test_data):
+        """Evaluate neural network
+
+        Evaluates performance of our neural network based on the test data
+
+        Args:
+            self (Network) : Object
+            test_data (list of pairs) : Image and number on image
+
+        """
+
+        test_results = [(np.argmax(self.feedForward(a)), y) 
+                        for (a, y) in test_data]
+        
+        return sum(int(y_predicted == y_true) for (y_predicted, y_true) in
+                    test_results)
     
-    # funckja aktywacyjna
     def cost_derivative(self, output_activations, y):
+        """Cost Derivative
+        Args:
+            self (Network) : Object
+            output_activations (float list) : Outputs from neural network
+            y (float list) : Expected outputs
+
+        returns:
+            float list : Derative of cost_derivative function
+        """
         return (output_activations - y)
     
-# funcjja sigmoid
+
 def sigmoid(z):
+    """"Sigmoid function
+
+    Args:
+        z (float) : Input number
+
+    returns:
+        float : Sigmoid of input number
+
+    """
     return 1.0/(1.0+np.exp(-z))
 
-# jakis dziwny sigmoid, nie rozumiem go
-def sigmoid_prime(z):
+
+def sigmoid_derivative(z):
+    """Derative of Sigmoid function
+
+    Args:
+        z (float) : Input number
+
+    returns:
+        float : Derative of Sigmoid function
+    """
     return sigmoid(z)*(1-sigmoid(z))
--- Uczenie maszynowe semestr 5/Lista 2/test.py
+++ Uczenie maszynowe semestr 5/Lista 2/test.py
@@ -1,10 +1,10 @@
 import mnist_loader
 from Neural_Network import Network 
 
-net = Network([784,30,10])
+network = Network([784, 40, 30, 10])
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 
-net.SGD(training_data,30,10,3,test_data = test_data)
+network.StochasticGradientDescent(training_data, 50, 5, 1, test_data=test_data)
 
 
 
