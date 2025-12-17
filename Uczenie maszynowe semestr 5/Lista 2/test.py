5494c790bcd12fbd4ff83e1c5f764978d2d3c28d
Kacper <kacperszmgielski123@gmail.com>
2025-12-17 01:27:47 +0100

    IDQ

--- Uczenie maszynowe semestr 5/Lista 2/test.py
+++ Uczenie maszynowe semestr 5/Lista 2/test.py
@@ -1,10 +1,10 @@
 import mnist_loader
 from Neural_Network import Network 
 
-network = Network([784, 40, 30, 10])
+network = Network([784, 128, 64, 10])
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 
-network.StochasticGradientDescent(training_data, 50, 5, 1, test_data=test_data)
+network.StochasticGradientDescent(training_data, 10, 10, 1, test_data=test_data)
 
 
 
